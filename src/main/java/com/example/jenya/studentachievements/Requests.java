fb18039f9c6b3d4e2ea01a31a6815b14da2f2f9c
Danil <wav9@yandex.ru>
2019-12-05 20:34:26 +0700

    Убрал разделители

--- src/main/java/com/example/jenya/studentachievements/Requests.java
+++ src/main/java/com/example/jenya/studentachievements/Requests.java
@@ -24,7 +24,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Requests {
-    private static final String URL = "https://aafbc70f.ngrok.io";
+    private static final String URL = "http://2920545b.ngrok.io";
     private final UserApi userApi;
     private static Requests instance;
 
