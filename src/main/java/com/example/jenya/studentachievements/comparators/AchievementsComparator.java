496b8832e48470ada131082c6716ec00ad52c5f8
Danil <wav9@yandex.ru>
2020-01-09 20:38:32 +0700

    Сортировка достижений (выполненные - невыполненные, дата + название)

--- src/main/java/com/example/jenya/studentachievements/comparators/AchievementsComparator.java
+++ src/main/java/com/example/jenya/studentachievements/comparators/AchievementsComparator.java
@@ -1,12 +1,19 @@
 package com.example.jenya.studentachievements.comparators;
 
 import com.example.jenya.studentachievements.models.Achievement;
+import com.google.common.collect.ComparisonChain;
 
 import java.util.Comparator;
 
-public class AchievementsComparator implements Comparator<Achievement> {
+public class AchievementsComparator implements Comparator<Achievement>
+{
+    @Override
     public int compare(Achievement a1, Achievement a2)
     {
-        return Integer.compare(a2.isReceived(), a1.isReceived());
+        return ComparisonChain.start()
+                .compare(a2.isReceived(), a1.isReceived())
+                .compare(a2.getDate(), a2.getDate())
+                .compare(a1.getAchievementInfo().getName(), a2.getAchievementInfo().getName())
+                .result();
     }
 }
